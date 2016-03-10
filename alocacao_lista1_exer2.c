5ffa0a45f2c3c976a1fe42d13f311cc22b61af2a
Daniel Maicon Lauermann <danilauermann@msn.com>
2016-03-10 14:46:37 -0300

    Update alocacao_lista1_exer2.c

--- alocacao_lista1_exer2.c
+++ alocacao_lista1_exer2.c
@@ -1,18 +1,31 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-	
-
-	int n1,n2;
-	int *v1,*v2;
-	
-	scanf("%d %d",&n1, &n2);
-	
-	      v1 = (int *) malloc(n1*sizeof(int));
-        v2 = (int *) malloc(n2*sizeof(int));
-		
+int* UN(int *v1,int n1,int *v2, int n2){
+	int V[n1+n2];
+	int *U;
+	 
+	U = (int *) malloc((n1+n2)*sizeof(int));
+	int i,j;
+	for(j=0;j<n1;j++)  V[j] =  v1[j];   
+        for(i=0;i<n2;i++)  V[i+n1]=v2[i];
+        for(i=0;i<(n1+n2);i++)U[i]= V[i];
+        return U;	
 }
 
-///// TERMINARRRR //
+int main(){	
+	int n1, n2, i, j;
+	int *v1,*v2, *uniao;
+	printf("Digite o tamanho de cada vetor: ");
+	scanf("%d %d",&n1, &n2);	
+	
+	v1 = (int *) malloc(n1*sizeof(int));
+        v2 = (int *) malloc(n2*sizeof(int));    
+    
+        printf("Primeiro vetor: ");
+        for(i = 0; i < n1; i++) scanf("%d",&v1[i]);
+	printf("Segundo vetor: ");
+        for(i = 0; i < n2; i++) scanf("%d",&v2[i]);
+	uniao = UN(v1,n1,v2,n2);
+	for(i=0;i<(n2+n1);i++) printf("%d\t",uniao[i]);   
+ }
