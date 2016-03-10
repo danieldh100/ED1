ee19ab976d7c2dcb7b4919b9387e0eef415b5230
Daniel Maicon Lauermann <danilauermann@msn.com>
2016-03-09 22:13:12 -0300

    Update alocacao_lista1_exer2.c

--- alocacao_lista1_exer2.c
+++ alocacao_lista1_exer2.c
@@ -1,1 +1,18 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+
+	int n1,n2;
+	int *v1,*v2;
+	
+	scanf("%d %d",&n1, &n2);
+	
+	      v1 = (int *) malloc(n1*sizeof(int));
+        v2 = (int *) malloc(n2*sizeof(int));
+		
+}
+
+///// TERMINARRRR //
