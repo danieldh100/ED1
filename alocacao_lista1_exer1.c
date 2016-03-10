b28ff6c180a427586029bba5000ca66fa35ab8e0
Daniel Maicon Lauermann <danilauermann@msn.com>
2016-03-09 22:01:30 -0300

    Update alocacao_lista1_exer1.c

--- alocacao_lista1_exer1.c
+++ alocacao_lista1_exer1.c
@@ -5,20 +5,17 @@ int main (){
 	int n, aux, bytes=5, i ,j, stop=1;
 	int *nums = (int*) malloc(200);
 	char r;
-		
+
 	for (i=0; i<bytes;i++){
-		aux = scanf("%d",&nums[i]);		
-		
+		aux = scanf("%d",&nums[i]);
+
 		if (i==bytes-2){
 	     bytes+=5;
 		 nums = (int *) realloc(nums, bytes*sizeof(int));  // Realocar
 		}
-		if(stop % 5 == 0){
-			printf("Deseja parar ? s ou n: ");  // Alocar mais 5 ou não ?
-			scanf("%s",&r);
-				if(r == 's') break;
+		if(aux == 0){
+			free(nums);
+			return 0;
 		}
-		stop++;
-	}	
-	for (j=0; j<=i; j++) printf("%d  ",nums[j]); //Imprimir
+	}
 }
